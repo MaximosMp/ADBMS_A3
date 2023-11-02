58553c42d92de56cd48665b16ed94e9b44e025b6
Constantinos Georgiou <kotsiosg40@gmail.com>
2023-11-02 15:14:29 +0100

    Update bin_script.py

--- bin_script.py
+++ bin_script.py
@@ -14,8 +14,8 @@ def bin_encode(input_file, encoding_width="int8"):
     # Create the output file name with the encoding acronym and width
     output_file = os.path.join("encoded_files/",os.path.basename(input_file).rsplit(".", 1)[0] + ".csv.bin")
 
-    mean_bits = calculate_max_bit_length(input_file)
-    encoding_width = choose_encoding_width(mean_bits)
+    bits = calculate_max_bit_length(input_file)
+    encoding_width = choose_encoding_width(bits)
 
     # Determine the format string based on the encoding width
     if encoding_width == "int8":
@@ -36,27 +36,7 @@ def bin_encode(input_file, encoding_width="int8"):
 
     return output_file
 
-def bin_decode(input_file, original_csv_file, decoding_width="int8"):
-
-
-    def compare_files_and_report(file1, file2):
-        with open(file1, 'r') as f1, open(file2, 'r') as f2:
-            lines1 = f1.readlines()
-            lines2 = f2.readlines()
-
-        # Perform a line-by-line comparison
-        d = difflib.Differ()
-        diff = list(d.compare(lines1, lines2))
-
-        # Check if the files match
-        files_match = all(line.startswith('  ') for line in diff)
-
-        return files_match, diff
-
-
-    mean_bits = calculate_max_bit_length(original_csv_file)
-    decoding_width = choose_encoding_width(mean_bits) 
-
+def bin_decode(input_file, decoding_width):
     # Define the format string based on the decoding width
     if decoding_width == "int8":
         format_string = 'b'
@@ -81,29 +61,9 @@ def bin_decode(input_file, original_csv_file, decoding_width="int8"):
             decoded_integers.append(value)
 
     # Create the output file path with the encoding acronym and width
-    output_file = os.path.join("output/",os.path.basename(input_file).rsplit(".", 1)[0] + ".bin.csv")
+    # output_file = os.path.join("output/",os.path.basename(input_file).rsplit(".", 1)[0] + ".bin.csv")
 
     # Print the decoded integers to the output file in the specified folder
-    with open(output_file, 'w') as f:
-        for value in decoded_integers:
-            f.write(str(value) + '\n')
-
-    # Compare the output CSV file with the original CSV file
-    files_match, differences = compare_files_and_report(output_file, original_csv_file)
-
-    if files_match:
-        print("The files match. No differences found.")
-    else:
-        print("The files do not match. Differences found.")
-        print("Differences:")
-        for line in differences:
-            if line.startswith('  '):
-                continue
-            elif line.startswith('- '):
-                print("Line removed:", line[2:])
-            elif line.startswith('+ '):
-                print("Line added:", line[2:])
-
-
-
-
+    # with open(output_file, 'w') as f:
+    for value in decoded_integers:
+        print(value)
