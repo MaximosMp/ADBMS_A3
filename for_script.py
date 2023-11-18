b07fe9b130162acc41214ab1e37f43d9b0871c2a
Constantinos Georgiou <kotsiosg40@gmail.com>
2023-11-18 18:13:58 +0100

    finalize the code for for_script.py

--- for_script.py
+++ for_script.py
@@ -1,14 +1,34 @@
-# %%
 import csv
 import numpy as np
+import os
+from helper import *
 
-# %% [markdown]
-# # INT 8
+def for_controller_encode(datatype, file):
+    output_dir_path = make_output_dir('encoded_files')
+    filename = os.path.basename(file) + '.for'
+    encoded_file = os.path.join(output_dir_path, filename)
+    
+    if datatype == 'int8':       
+        encoded_file = FrameOfReference_encoding_int8(file, encoded_file)
+    elif datatype == 'int16':
+        encoded_file = FrameOfReference_encoding_int16(file, encoded_file)
+    elif datatype == 'int32':
+        encoded_file = FrameOfReference_encoding_int32(file, encoded_file)
+    elif datatype == 'int64':
+        encoded_file = FrameOfReference_encoding_int64(file, encoded_file)
 
-# %% [markdown]
-# ### Encoding
 
-# %%
+def for_controller_decode(datatype, file):
+    if datatype == 'int8':
+        FrameOfReference_decoding_int8(file)
+    elif datatype == 'int16':
+        FrameOfReference_decoding_int16(file)
+    elif datatype == 'int32':
+        FrameOfReference_decoding_int32(file)
+    elif datatype == 'int64':
+        FrameOfReference_decoding_int64(file)
+
+
 def FrameOfReference_encoding_int8(input_file, output_file):
     #first we read the first 200 lines
     sample = []
@@ -33,7 +53,7 @@ def FrameOfReference_encoding_int8(input_file, output_file):
             
             if count == 0:
                 #running_value = current_value
-                binary_out.write(frame.to_bytes(length=1, byteorder='big', signed=True))
+                binary_out.write(frame.to_bytes(length=1, byteorder='big', signed=False))
                 
             if -127 <= diff <= 127:
                 binary_out.write(diff.to_bytes(length=1, byteorder='big', signed=True))
@@ -41,34 +61,22 @@ def FrameOfReference_encoding_int8(input_file, output_file):
 
             else:
                 binary_out.write(escape_code)
-                binary_out.write(current_value.to_bytes(length=1, byteorder='big', signed=True))
+                binary_out.write(current_value.to_bytes(length=1, byteorder='big', signed=False))
 
             count+=1
 
-# %%
-# input_csv_file = 'ADM-2023-Assignment-3-data-TPCH-SF-1\l_tax-int8.csv'
-# output_dif_file = 'encoded_output.csv.dif'
-
-# FrameOfReference_encoding_int8(input_csv_file, output_dif_file)
-
-#from 11,722 KB to 5,861 KB
-
-# %% [markdown]
-# ### Decoding
-
-# %%
-def FrameOfReference_decoding_int8(input_file, output_file):
+def FrameOfReference_decoding_int8(input_file):
     first_row = True
     escape = 0
 
-    with open(input_file, 'rb') as binary_in, open(output_file, 'w') as text_out:
+    with open(input_file, 'rb') as binary_in:
         while True:
             if first_row:
                 chunk = binary_in.read(1)
                 if not chunk:
                     break
 
-                frame = int.from_bytes(chunk, byteorder = 'big', signed = True)
+                frame = int.from_bytes(chunk, byteorder = 'big', signed = False)
 
                 first_row = False
             else:
@@ -76,9 +84,11 @@ def FrameOfReference_decoding_int8(input_file, output_file):
                     chunk = binary_in.read(1)
                     if not chunk:
                         break
-                    current_value = int.from_bytes(chunk, byteorder= 'big', signed=True)
+                    current_value = int.from_bytes(chunk, byteorder= 'big', signed=False)
                     
-                    text_out.write(f"{current_value}\n")
+                    print(f"{current_value}")
+
+                    escape = 0
                     
                 else:
                     chunk = binary_in.read(1)
@@ -88,25 +98,10 @@ def FrameOfReference_decoding_int8(input_file, output_file):
     
                     if -127 <= current_value <= 127:
                         diff = frame + current_value
-                        text_out.write(f"{diff}\n")
+                        print(f"{diff}")
                     else:
                         escape = 1
 
-# %%
-# input_dif_file = 'encoded_output.csv.dif'
-# output_csv_file = 'decoded_output.csv.dif.csv'
-
-# FrameOfReference_decoding_int8(input_dif_file, output_csv_file)
-
-# back to 11,722 KB
-
-# %% [markdown]
-# # INT 16
-
-# %% [markdown]
-# ### Encoding
-
-# %%
 def FrameOfReference_encoding_int16(input_file, output_file):
     #first we read the first 200 lines
     sample = []
@@ -131,7 +126,7 @@ def FrameOfReference_encoding_int16(input_file, output_file):
             
             if count == 0:
                 #running_value = current_value
-                binary_out.write(frame.to_bytes(length=2, byteorder='big', signed=True))
+                binary_out.write(frame.to_bytes(length=2, byteorder='big', signed=False))
                 
             if -127 <= diff <= 127:
                 binary_out.write(diff.to_bytes(length=1, byteorder='big', signed=True))
@@ -139,34 +134,22 @@ def FrameOfReference_encoding_int16(input_file, output_file):
 
             else:
                 binary_out.write(escape_code)
-                binary_out.write(current_value.to_bytes(length=2, byteorder='big', signed=True))
+                binary_out.write(current_value.to_bytes(length=2, byteorder='big', signed=False))
 
             count+=1
 
-# %%
-# input_csv_file = 'ADM-2023-Assignment-3-data-TPCH-SF-1\l_discount-int16.csv'
-# output_dif_file = 'encoded_output.csv.dif'
-
-# FrameOfReference_encoding_int16(input_csv_file, output_dif_file)
-
-#from 12,255 KB to 5,861 KB
-
-# %% [markdown]
-# ### Decoding
-
-# %%
-def FrameOfReference_decoding_int16(input_file, output_file):
+def FrameOfReference_decoding_int16(input_file):
     first_row = True
     escape = 0
 
-    with open(input_file, 'rb') as binary_in, open(output_file, 'w') as text_out:
+    with open(input_file, 'rb') as binary_in:
         while True:
             if first_row:
                 chunk = binary_in.read(2)
                 if not chunk:
                     break
 
-                frame = int.from_bytes(chunk, byteorder = 'big', signed = True)
+                frame = int.from_bytes(chunk, byteorder = 'big', signed = False)
 
                 first_row = False
             else:
@@ -174,9 +157,11 @@ def FrameOfReference_decoding_int16(input_file, output_file):
                     chunk = binary_in.read(2)
                     if not chunk:
                         break
-                    current_value = int.from_bytes(chunk, byteorder= 'big', signed=True)
+                    current_value = int.from_bytes(chunk, byteorder= 'big', signed=False)
                     
-                    text_out.write(f"{current_value}\n")
+                    print(f"{current_value}")
+
+                    escape = 0
                     
                 else:
                     chunk = binary_in.read(1)
@@ -186,25 +171,12 @@ def FrameOfReference_decoding_int16(input_file, output_file):
     
                     if -127 <= current_value <= 127:
                         diff = frame + current_value
-                        text_out.write(f"{diff}\n")
+                        print(f"{diff}")
                     else:
                         escape = 1
 
-# %%
-# input_dif_file = 'encoded_output.csv.dif'
-# output_csv_file = 'decoded_output.csv.dif.csv'
 
-# FrameOfReference_decoding_int16(input_dif_file, output_csv_file)
 
-# back to 12,255 KB
-
-# %% [markdown]
-# # INT 32
-
-# %% [markdown]
-# ### Encoding
-
-# %%
 def FrameOfReference_encoding_int32(input_file, output_file):
     #first we read the first 200 lines
     sample = []
@@ -218,6 +190,7 @@ def FrameOfReference_encoding_int32(input_file, output_file):
                 break
 
     frame = int(np.mean(sample))
+    
 
     sample_diff = []
     for i in sample:
@@ -227,7 +200,8 @@ def FrameOfReference_encoding_int32(input_file, output_file):
         escape_length = 1
     else:
         escape_length = 2
-        
+
+    #print(frame, escape_length)    
     count = 0
     escape_code = int(-2**((8*escape_length)-1)).to_bytes(length=escape_length, byteorder='big',signed=True)  # Special escape code for differences
     min_diff = -2**((8*escape_length)-1)+1
@@ -241,38 +215,34 @@ def FrameOfReference_encoding_int32(input_file, output_file):
             if count == 0:
                 #running_value = current_value
                 binary_out.write(escape_length.to_bytes(length=1, byteorder='big', signed=True)) ## write escape code
-                binary_out.write(frame.to_bytes(length=3, byteorder='big', signed=True))
+                binary_out.write(frame.to_bytes(length=3, byteorder='big', signed=False))
+
+                #print(0, escape_length.to_bytes(length=1, byteorder='big', signed=True))
+                #print(1, frame.to_bytes(length=3, byteorder='big', signed=False))
                 
             if min_diff <= diff <= max_diff:
                 binary_out.write(diff.to_bytes(length=escape_length, byteorder='big', signed=True))
+                #print(2, diff.to_bytes(length=escape_length, byteorder='big', signed=True))
                 #running_value = current_value
 
             else:
                 binary_out.write(escape_code)
-                binary_out.write(current_value.to_bytes(length=3, byteorder='big', signed=True))
+                #print(2, escape_code)
+                #print(3, current_value, current_value.to_bytes(length=3, byteorder='big', signed=False))
+                binary_out.write(current_value.to_bytes(length=3, byteorder='big', signed=False))
 
             count+=1
 
-# %%
-# input_csv_file = 'ADM-2023-Assignment-3-data-TPCH-SF-1\l_suppkey-int32.csv'
-# output_dif_file = 'encoded_output.csv.dif'
 
-# FrameOfReference_encoding_int32(input_csv_file, output_dif_file)
-
-#from 28,655 KB to 11,722 KB
-
-# %% [markdown]
-# ### Decoding
-
-# %%
-def FrameOfReference_decoding_int32(input_file, output_file):
+def FrameOfReference_decoding_int32(input_file):
     first_row = True
     escape = 0
 
-    with open(input_file, 'rb') as binary_in, open(output_file, 'w') as text_out:
+    with open(input_file, 'rb') as binary_in:
         while True:
             if first_row:
                 chunk = binary_in.read(1)
+                #print(0, chunk)
                 if not chunk:
                     break
 
@@ -284,45 +254,38 @@ def FrameOfReference_decoding_int32(input_file, output_file):
                 if not chunk:
                     break
 
-                frame = int.from_bytes(chunk, byteorder = 'big', signed = True)
-
+                frame = int.from_bytes(chunk, byteorder = 'big', signed = False)
+                #print(1, chunk)
+                
                 first_row = False
             else:
                 if escape == 1:
                     chunk = binary_in.read(3)
+                    #print(3, chunk)
                     if not chunk:
                         break
-                    current_value = int.from_bytes(chunk, byteorder= 'big', signed=True)
+                    current_value = int.from_bytes(chunk, byteorder= 'big', signed=False)
+                    #print(current_value)
                     
-                    text_out.write(f"{current_value}\n")
+                    print(f"{current_value}")
+
+                    escape = 0
                     
                 else:
                     chunk = binary_in.read(escape_length)
+                    #print(2,chunk)
                     if not chunk:
                         break
                     current_value = int.from_bytes(chunk, byteorder = 'big', signed = True)
+                    #print(current_value)
     
                     if min_diff <= current_value <= max_diff:
                         diff = frame + current_value
-                        text_out.write(f"{diff}\n")
+                        #print(frame, current_value, diff)
+                        print(f"{diff}")
                     else:
                         escape = 1
 
-# %%
-# input_dif_file = 'encoded_output.csv.dif'
-# output_csv_file = 'decoded_output.csv.dif.csv'
-
-# FrameOfReference_decoding_int32(input_dif_file, output_csv_file)
-
-# back to 28,655 KB
-
-# %% [markdown]
-# # INT 64
-
-# %% [markdown]
-# ### Encoding
-
-# %%
 def FrameOfReference_encoding_int64(input_file, output_file):
     #first we read the first 200 lines
     sample = []
@@ -361,7 +324,7 @@ def FrameOfReference_encoding_int64(input_file, output_file):
             if count == 0:
                 #running_value = current_value
                 binary_out.write(escape_length.to_bytes(length=1, byteorder='big', signed=True)) ## write escape code
-                binary_out.write(frame.to_bytes(length=4, byteorder='big', signed=True))
+                binary_out.write(frame.to_bytes(length=4, byteorder='big', signed=False))
                 
             if min_diff <= diff <= max_diff:
                 binary_out.write(diff.to_bytes(length=escape_length, byteorder='big', signed=True))
@@ -369,27 +332,15 @@ def FrameOfReference_encoding_int64(input_file, output_file):
 
             else:
                 binary_out.write(escape_code)
-                binary_out.write(current_value.to_bytes(length=4, byteorder='big', signed=True))
+                binary_out.write(current_value.to_bytes(length=4, byteorder='big', signed=False))
 
             count+=1
 
-# %%
-# input_csv_file = 'ADM-2023-Assignment-3-data-TPCH-SF-1\l_extendedprice-int64.csv'
-# output_dif_file = 'encoded_output.csv.dif'
-
-# FrameOfReference_encoding_int64(input_csv_file, output_dif_file)
-
-# from 46,131 KB to 17,582 KB
-
-# %% [markdown]
-# ### Decoding
-
-# %%
-def FrameOfReference_decoding_int64(input_file, output_file):
+def FrameOfReference_decoding_int64(input_file):
     first_row = True
     escape = 0
 
-    with open(input_file, 'rb') as binary_in, open(output_file, 'w') as text_out:
+    with open(input_file, 'rb') as binary_in:
         while True:
             if first_row:
                 chunk = binary_in.read(1)
@@ -404,7 +355,7 @@ def FrameOfReference_decoding_int64(input_file, output_file):
                 if not chunk:
                     break
 
-                frame = int.from_bytes(chunk, byteorder = 'big', signed = True)
+                frame = int.from_bytes(chunk, byteorder = 'big', signed = False)
 
                 first_row = False
             else:
@@ -412,9 +363,11 @@ def FrameOfReference_decoding_int64(input_file, output_file):
                     chunk = binary_in.read(4)
                     if not chunk:
                         break
-                    current_value = int.from_bytes(chunk, byteorder= 'big', signed=True)
+                    current_value = int.from_bytes(chunk, byteorder= 'big', signed=False)
                     
-                    text_out.write(f"{current_value}\n")
+                    print(f"{current_value}")
+
+                    escape = 0
                     
                 else:
                     chunk = binary_in.read(escape_length)
@@ -424,16 +377,6 @@ def FrameOfReference_decoding_int64(input_file, output_file):
     
                     if min_diff <= current_value <= max_diff:
                         diff = frame + current_value
-                        text_out.write(f"{diff}\n")
+                        print(f"{diff}")
                     else:
                         escape = 1
-
-# %%
-# input_dif_file = 'encoded_output.csv.dif'
-# output_csv_file = 'decoded_output.csv.dif.csv'
-
-# FrameOfReference_decoding_int64(input_dif_file, output_csv_file)
-
-# back to 46,131 KB
-
-
